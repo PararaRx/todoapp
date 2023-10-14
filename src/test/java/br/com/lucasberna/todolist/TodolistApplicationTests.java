8375be2fad089e9c4f5df10a98f0c5263289ac4e
Parara <lucas.sp102@gmail.com>
2023-10-14 02:18:25 -0300

    DockerFile Deploy

---
+++ src/test/java/br/com/lucasberna/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasberna.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
